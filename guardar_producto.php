d658fe21484f82f7adc5e5ee3942600e33fb45a8
Iván Narváez <77557663+calebe320@users.noreply.github.com>
2024-04-09 17:01:26 -0600

    guardar_producto.php

--- guardar_producto.php
+++ guardar_producto.php
@@ -2,20 +2,23 @@
 // Conexión a la base de datos
 $servername = "localhost";
 $dbname = "tiendajuguetes";
+$username = "";
+$password = "";
 
-$conn = new mysqli($servername, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-
-// Recibir datos del formularios
+// Recibir datos del formulario
 $nombre = $_POST['productName'];
 $descripcion = $_POST['productDescription'];
 $precio = $_POST['productPrice'];
 $cantidad = $_POST['productQuantity'];
 $proveedor = $_POST['productSupplier'];
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 
 // Insertar datos en la tabla de productos
 $sql = "INSERT INTO productos (Nombre, Descripcion, Precio, Cantidad_Stock, Proveedor)
